5f8ccbb449f885e17f08c62a446d1995fcadc5f0
exit02 <95013304+exit02@users.noreply.github.com>
2022-01-17 09:23:56 +0900

    45.Stateを知る(useState)

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import ColorfulMessage from "./components/ColorfulMessage";
 
 // ファイル名の拡張子は.js または、.jsxとする。
@@ -11,10 +11,15 @@ import ColorfulMessage from "./components/ColorfulMessage";
 const App = () => {
   // ボタンイベントの関数
   // <button>内のイベント名はキャメルケースの書き方をする。onClick 途中に大文字が入る。
-  const onClickButton = () => alert();
-  // オブジェクトを定義できる
+  const onClickCountUp = () => {
+    setNum(num + 1);
+  };
+  // useStateの指定（状態変化の取得、設定？が行える）
+  // 配列で変数と関数名を取得する。変数名、関数名は自分で決められる。
+  // useStateの引数は初期値を指定する。
+  const [num, setNum] = useState(0);
   return (
-    // returnするhtmlは１つのタグで返却しなくてはいけない。
+    // returnするhtmlは１つのタグで返却しなくてはいけない。<>
     // また、タグを記載する場合divタグを使用すると返却先のhtmlに反映されてしまうため、
     // 不要なdivタグを反映させない方法として、React.fragmentを使用する。
     // または、React.fragmentの代わりに空のタグ<>を記載するとこで同じ動作をする。
@@ -25,7 +30,8 @@ const App = () => {
       <h1 style={{ color: "red" }}>こんにちは！</h1>
       <ColorfulMessage color="blue">お元気ですか？</ColorfulMessage>
       <ColorfulMessage color="pink">元気です！</ColorfulMessage>
-      <button onClick={onClickButton}>ボタン</button>
+      <button onClick={onClickCountUp}>カウントアップ！</button>
+      <p>{num}</p>
     </>
   );
 };
